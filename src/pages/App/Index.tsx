7cc5ec15e2ddb2638edb6630795ecf4cf0013d22
Alisson Sanches <105376629+AliSanches@users.noreply.github.com>
2024-12-02 13:22:27 -0300

    Fix - NavigationRoutes - Frontend

--- src/pages/App/Index.tsx
+++ src/pages/App/Index.tsx
@@ -5,9 +5,11 @@ import Nav from "react-bootstrap/Nav";
 import style from "./style.module.css";
 import { MenuMobile } from "./MenuMobile";
 
-import { Outlet } from "react-router";
+import { useNavigate, Outlet } from "react-router";
 
 export const Index = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="container-fluid p-0">
       <div className="container-lg d-flex justify-content-center my-2">
@@ -19,37 +21,55 @@ export const Index = () => {
       >
         <div className="d-none d-md-flex justify-content-center align-items-center gap-5">
           <Nav.Item>
-            <Nav.Link className={style.nav} href="/app/home">
+            <Nav.Link
+              className={style.nav}
+              onClick={() => navigate("/app/home")}
+            >
               HOME
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={style.nav} href="/app/curso">
+            <Nav.Link
+              className={style.nav}
+              onClick={() => navigate("/app/curso")}
+            >
               CURSO
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={style.nav} href="/app/turma">
+            <Nav.Link
+              className={style.nav}
+              onClick={() => navigate("/app/turma")}
+            >
               TURMA
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={style.nav} href="/app/sala">
+            <Nav.Link
+              className={style.nav}
+              onClick={() => navigate("/app/sala")}
+            >
               SALA
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={style.nav} href="/app/reserva">
+            <Nav.Link
+              className={style.nav}
+              onClick={() => navigate("/app/reserva")}
+            >
               RESERVA
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={style.nav} href="/app/usuarios">
+            <Nav.Link
+              className={style.nav}
+              onClick={() => navigate("/app/usuarios")}
+            >
               USUÁRIOS
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link className={style.nav} href="/">
+            <Nav.Link className={style.nav} onClick={() => navigate("/")}>
               SAIR
             </Nav.Link>
           </Nav.Item>
