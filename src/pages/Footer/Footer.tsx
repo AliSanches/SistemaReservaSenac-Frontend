8a764cf4a1eaf2f01d17f54f2ef3206bc86b5fa8
Alisson Sanches <105376629+AliSanches@users.noreply.github.com>
2024-12-03 08:45:39 -0300

    Fix - FooterSize - Frontend

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,7 +1,9 @@
+import style from "./Footer.module.css";
+
 export const Footer = () => {
   return (
     <footer
-      className="container-fluid bg-primary d-flex justify-content-center align-items-center text-white position-fixed bottom-0 "
+      className={`container-fluid bg-primary d-flex justify-content-center align-items-center text-white position-fixed bottom-0 ${style.fs_12}`}
       style={{ height: "50px" }}
     >
       © Copyright 2025 - Todos os direitos reservados
